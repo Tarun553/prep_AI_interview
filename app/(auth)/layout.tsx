505f751b560fd4e7160f8ffa8612c776b6ec230e
tarun07 <tarunchoudhary553@gmail.com>
2025-04-12 02:45:41 +0530

    final commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import { isAuthenticated } from "@/lib/actions/auth.actions";
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
-  if (isUserAuthenticated) redirect("/");
+  if (isUserAuthenticated) redirect("/interview");
 
   return <div className="auth-layout">{children}</div>;
 };
